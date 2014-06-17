1586149d843ecc9006794f2b336c8cd186f5b279
karim3461 <karim3461@gmail.com>
2014-06-17 15:40:01 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 ## matrix that can store/cache its inverse. This special matrix defines also a list of 4 functions that 
 ## allow respectively to set the input matrix, to retrieve it, to set the inverse of the input matrix,  
 ## and to retrieve it.
-## The second fucntion "cacheSolve" computes the actual inverse of the input matrix and stores it in an 
+## The second function "cacheSolve" computes the actual inverse of the input matrix and stores it in an 
 ## internal variable that acts as a cache. This is done by invoking the inner functions of the special
 ## matrix created by "makecacheMatrix".
 
